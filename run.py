49bc7f278a25765546b33277af4747e530f3a194
OyugoObonyo <nyikal1999@gmail.com>
2022-07-10 18:33:39 +0300

    Commence major refactoring

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-# This will be the program's entry point
+from run import get_app, get_db
+import os
+
+app = get_app()
+db = get_db()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- src/ProductMicroservice/run.py
+++
@@ -1,8 +0,0 @@
-from app import get_app, get_db
-import os
-
-app = get_app()
-db = get_db()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
