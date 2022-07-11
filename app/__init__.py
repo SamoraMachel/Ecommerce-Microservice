794013252275b4313f14455c9099369ef640d94a
OyugoObonyo <nyikal1999@gmail.com>
2022-07-11 11:01:00 +0300

    Delete unused modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from app import get_app, get_db
+from run import get_app, get_db
 
 app : Flask = get_app()
 db : SQLAlchemy = get_db()
